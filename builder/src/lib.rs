7da6588fc808526a1ea6cbdef4d2403531ddeee6
wjl <william.lewis112358@gmail.com>
2020-12-13 11:31:08 -0500

    Pass 02-create-builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,9 +1,47 @@
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput};
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, Data, DeriveInput, Field};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _input = parse_macro_input!(input as DeriveInput);
+    let input = parse_macro_input!(input as DeriveInput);
 
-    TokenStream::new()
+    let struct_ident = input.ident;
+    let builder_ident = format_ident!("{}Builder", struct_ident);
+
+    if let Data::Struct(data) = input.data {
+        // Here we essentially copy the fields from the original struct,
+        // wrapping the types in `Option<..>`s in the process.
+        let fields = data.fields.iter().map(|field| {
+            let Field { ident, ty, .. } = field;
+            quote! {
+                #ident: Option<#ty>
+            }
+        });
+
+        let init_values = data.fields.iter().map(|field| {
+            let Field { ident, .. } = field;
+            quote! {
+                #ident: None
+            }
+        });
+
+        let expanded = quote! {
+            pub struct #builder_ident {
+                #(#fields),*
+            }
+
+            impl #struct_ident {
+                fn builder() -> #builder_ident {
+                    #builder_ident {
+                        #(#init_values),*
+                    }
+                }
+            }
+        };
+
+        TokenStream::from(expanded)
+    } else {
+        panic!("Expected a struct")
+    }
 }
